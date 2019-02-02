d22f93881571cdc22e8883acb51a4b2674923434
bykwo <bykwon223@gmail.com>
2019-02-02 19:57:01 +0900

    Initial commit

---
+++ app/src/main/java/com/bobo/min/fucusCam/MainActivity.java
@@ -0,0 +1,21 @@
+package com.bobo.min.fucusCam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aryan.dhankar.customcamera.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+
+    }
+}
